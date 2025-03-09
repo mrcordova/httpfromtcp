7035e3c421bbafca3c924dc681aac579a03bf03f
mrcordova <46950394+mrcordova@users.noreply.github.com>
2025-03-09 11:28:29 -0700

    read from file

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/mrcordova/httpfromtcp
+
+go 1.24.0
---
+++ main.go
@@ -0,0 +1,35 @@
+package main
+
+import (
+	"errors"
+	"fmt"
+	"io"
+	"log"
+	"os"
+)
+
+const inputFilePath = "messages.txt"
+
+func main() {
+	f, err := os.Open(inputFilePath)
+	if err != nil {
+		log.Fatalf("could not open %s: %s\n", inputFilePath, err)
+	}
+
+	fmt.Printf("Reading data from %s\n", inputFilePath)
+	fmt.Println("=====================================")
+
+	for {
+		b := make([]byte, 8, 8)
+		n, err := f.Read(b)
+		if err != nil {
+			if errors.Is(err, io.EOF) {
+				break
+			}
+			fmt.Printf("error: %s\n", err.Error())
+			break
+		}
+		str := string(b[:n])
+		fmt.Printf("read: %s\n", str)
+	}
+}
