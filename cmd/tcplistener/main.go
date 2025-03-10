73b378f8ace3bb605546e24354f2dc546b96431d
mrcordova <46950394+mrcordova@users.noreply.github.com>
2025-03-10 13:52:27 -0700

    use RequestFromReader in tdplistner

--- cmd/tcplistener/main.go
+++ cmd/tcplistener/main.go
@@ -1,12 +1,11 @@
 package main
 
 import (
-	"errors"
 	"fmt"
-	"io"
 	"log"
 	"net"
-	"strings"
+
+	"github.com/mrcordova/httpfromtcp/internal/request"
 )
 
 
@@ -28,42 +27,15 @@ func main() {
 		}
 		fmt.Println("Accepted connection from", conn.RemoteAddr())
 
-		linesChan := getLinesChannel(conn)
-
-		for line := range linesChan {
-			fmt.Println(line)
+		requestLine, err := request.RequestFromReader(conn)
+		if err != nil {
+			log.Fatalf("failed to get requestline")
 		}
-		fmt.Println("Connection to ", conn.RemoteAddr(), "closed")
+		fmt.Println("Request line:")
+		fmt.Printf("- Method: %s\n", requestLine.RequestLine.Method)
+		fmt.Printf("- Target: %s\n", requestLine.RequestLine.RequestTarget)
+		fmt.Printf("- Version: %s\n", requestLine.RequestLine.HttpVersion)
+	
 	}
 }
 
-func getLinesChannel(f io.ReadCloser) <-chan string {
-	lines := make(chan string)
-	go func() {
-		defer f.Close()
-		defer close(lines)
-		currentLineContents := ""
-		for {
-			b := make([]byte, 8, 8)
-			n, err := f.Read(b)
-			if err != nil {
-				if currentLineContents != "" {
-					lines <- currentLineContents
-				}
-				if errors.Is(err, io.EOF) {
-					break
-				}
-				fmt.Printf("error: %s\n", err.Error())
-				return
-			}
-			str := string(b[:n])
-			parts := strings.Split(str, "\n")
-			for i := range len(parts)-1 {
-				lines <- fmt.Sprintf("%s%s", currentLineContents, parts[i])
-				currentLineContents = ""
-			}
-			currentLineContents += parts[len(parts)-1]
-		}
-	}()
-	return lines
-}
